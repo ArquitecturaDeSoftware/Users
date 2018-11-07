cd1a19fc6e526485e315474133e9c0954d7ddfcd
Juan Castelblanco <jscastelblancoh@unal.edu.co>
2018-11-06 23:10:02 -0500

    correccion update user y adicionar password

--- user/pkg/db/db.go
+++ user/pkg/db/db.go
@@ -5,7 +5,7 @@ import (
 )
 
 var mgoSession *mgo.Session
-var mongo_conn_str = "mongodb:27017"
+var mongo_conn_str = "mongodb:27017"  
 
 // Creates a new session if mgoSession is nil i.e there is no active mongo session.
 //If there is an active mongo session it will return a Clone
